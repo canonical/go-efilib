10482a7c9e43168598c5d373159fbeac78cef12b
Chris Coulson <chris.coulson@canonical.com>
2021-07-07 20:33:03 +0100

    Test the device path returned from ReadDevicePath, not just the string version of it

--- devicepath_test.go
+++ devicepath_test.go
@@ -24,6 +24,28 @@ func (s *dpSuite) TestReadDevicePath(c *C) {
 	c.Assert(err, IsNil)
 	c.Check(path.String(), Equals, "\\PciRoot(0x0)\\Pci(0x1d,0x0)\\Pci(0x0,0x0)\\NVMe(0x1-0x00-0x00-0x00-0x00-0x00-0x00-0x00-0x00)"+
 		"\\HD(1,GPT,66de947b-fdb2-4525-b752-30d66bb2b960,0x0000000000000800,0x0000000000100000)\\\\EFI\\ubuntu\\shimx64.efi")
+
+	expected := DevicePath{
+		&ACPIDevicePathNode{
+			HID: 0x0a0341d0,
+			UID: 0x0},
+		&PCIDevicePathNode{
+			Function: 0x0,
+			Device:   0x1d},
+		&PCIDevicePathNode{
+			Function: 0x0,
+			Device:   0x0},
+		&NVMENamespaceDevicePathNode{
+			NamespaceID:   0x1,
+			NamespaceUUID: 0x0},
+		&HardDriveDevicePathNode{
+			PartitionNumber: 1,
+			PartitionStart:  0x800,
+			PartitionSize:   0x100000,
+			Signature:       MakeGUID(0x66de947b, 0xfdb2, 0x4525, 0xb752, [...]uint8{0x30, 0xd6, 0x6b, 0xb2, 0xb9, 0x60}),
+			MBRType:         GPT},
+		&FilePathDevicePathNode{PathName: "\\EFI\\ubuntu\\shimx64.efi"}}
+	c.Check(path, DeepEquals, expected)
 }
 
 func (s *dpSuite) TestWriteDevicePath(c *C) {
