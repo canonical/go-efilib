3579942f9faf10671b43eeca64d608118d1528ff
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2021-09-09 11:03:15 +0100

    test: use unsafe to assign EFIVARFS_MAGIC

--- vars_linux_test.go
+++ vars_linux_test.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"syscall"
 	"time"
+	"unsafe"
 
 	"golang.org/x/sys/unix"
 
@@ -266,7 +267,7 @@ func (s *varsLinuxSuite) TestProbeEfivarfs(c *C) {
 		if err := unix.Statfs("testdata", st); err != nil {
 			return err
 		}
-		st.Type = unix.EFIVARFS_MAGIC
+		*(*uint)(unsafe.Pointer(&st.Type)) = uint(unix.EFIVARFS_MAGIC)
 		return nil
 	})
 	defer restore()
