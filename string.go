5444845d0375eff92ccd780c2ff5c60f3de62b6a
Chris Coulson <chris.coulson@canonical.com>
2021-09-17 20:54:49 +0100

    Add a helper for converting UTF8 to UCS2

--- string.go
+++ string.go
@@ -9,9 +9,9 @@ import (
 	"unicode/utf8"
 )
 
-// ConvertUTF16ToUTF8 converts the supplied UTF-16 string to a
-// UTF-8 string. If the supplied string is NULL-terminated, then
-// the NULL termination is removed from the string.
+// ConvertUTF16ToUTF8 converts the supplied UTF-16 or UCS2 string
+// to a UTF-8 string. If the supplied string is NULL-terminated,
+// then the NULL termination is removed from the string.
 func ConvertUTF16ToUTF8(in []uint16) string {
 	var u8 []byte
 	for _, r := range utf16.Decode(in) {
@@ -31,6 +31,22 @@ func ConvertUTF8ToUTF16(in string) []uint16 {
 	var unicodeStr []rune
 	for len(in) > 0 {
 		r, sz := utf8.DecodeRuneInString(in)
+		unicodeStr = append(unicodeStr, r)
+		in = in[sz:]
+	}
+	return utf16.Encode(unicodeStr)
+}
+
+// ConvertUTF8ToUCS2 converts the supplied UTF-8 string to a
+// UCS2 string. Any code point outside of the BML is converted
+// to the replacement character.
+func ConvertUTF8ToUCS2(in string) []uint16 {
+	var unicodeStr []rune
+	for len(in) > 0 {
+		r, sz := utf8.DecodeRuneInString(in)
+		if r >= 0x10000 {
+			r = '\uFFFD'
+		}
 		unicodeStr = append(unicodeStr, r)
 		in = in[sz:]
 	}
---
+++ string_test.go
@@ -0,0 +1,57 @@
+// Copyright 2021 Canonical Ltd.
+// Licensed under the LGPLv3 with static-linking exception.
+// See LICENCE file for details.
+
+package efi_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	. "github.com/canonical/go-efilib"
+)
+
+type stringSuite struct{}
+
+var _ = Suite(&stringSuite{})
+
+func (s *stringSuite) TestUTF16ConversionASCII(c *C) {
+	u8 := "abcdefg"
+	u16 := ConvertUTF8ToUTF16(u8)
+	c.Check(u16, DeepEquals, []uint16{0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, u8)
+}
+
+func (s *stringSuite) TestUCS2ConversionASCII(c *C) {
+	u8 := "abcdefg"
+	u16 := ConvertUTF8ToUCS2(u8)
+	c.Check(u16, DeepEquals, []uint16{0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, u8)
+}
+
+func (s *stringSuite) TestUTF16ConversionBML(c *C) {
+	u8 := "αβγδεζη"
+	u16 := ConvertUTF8ToUTF16(u8)
+	c.Check(u16, DeepEquals, []uint16{0x3b1, 0x3b2, 0x3b3, 0x3b4, 0x3b5, 0x3b6, 0x3b7})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, u8)
+}
+
+func (s *stringSuite) TestUCS2ConversionBML(c *C) {
+	u8 := "αβγδεζη"
+	u16 := ConvertUTF8ToUCS2(u8)
+	c.Check(u16, DeepEquals, []uint16{0x3b1, 0x3b2, 0x3b3, 0x3b4, 0x3b5, 0x3b6, 0x3b7})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, u8)
+}
+
+func (s *stringSuite) TestUTF16ConversionSymbols(c *C) {
+	u8 := "😸💩🌷❗"
+	u16 := ConvertUTF8ToUTF16(u8)
+	c.Check(u16, DeepEquals, []uint16{0xd83d, 0xde38, 0xd83d, 0xdca9, 0xd83c, 0xdf37, 0x2757})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, u8)
+}
+
+func (s *stringSuite) TestUCS2ConversionSymbols(c *C) {
+	u8 := "😸💩🌷❗"
+	u16 := ConvertUTF8ToUCS2(u8)
+	c.Check(u16, DeepEquals, []uint16{0xfffd, 0xfffd, 0xfffd, 0x2757})
+	c.Check(ConvertUTF16ToUTF8(append(u16, 0)), Equals, "���❗")
+}
