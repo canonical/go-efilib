6aad32eebc6845814173d3bbeff4abaa563d362b
Chris Coulson <chris.coulson@canonical.com>
2022-01-27 21:05:52 +0000

    linux: split the IDE and SATA handling code

--- linux/dp_ata.go
+++ linux/dp_ata.go
@@ -15,122 +15,52 @@ import (
 	"strings"
 
 	"golang.org/x/xerrors"
-
-	"github.com/canonical/go-efilib"
 )
 
-// ataRE matches an ATA path including the components from the SCSI subsystem,
-// capturing the ATA print ID, SCSI channel, SCSI target and SCSI LUN.
-//
-// The kernel creates one ata%d device per port (see
-// drivers/ata/libata-core.c:ata_host_register). Each ATA device is represented
-// in the SCSI layer by setting the SCSI channel to the port multiplier port
-// number, the SCSI target to the device number (0 for SATA, 0 or 1 for PATA),
-// and the SCSI LUN as the LUN (see drivers/ata/libata-scsi.c:ata_scsi_scan_host).
-var ataRE = regexp.MustCompile(`^ata([[:digit:]]+)\/host[[:digit:]]+\/target[[:digit:]]+\:[[:digit:]]+\:[[:digit:]]+\/[[:digit:]]+\:([[:digit:]]+)\:([[:digit:]])+\:([[:digit:]])+\/block\/s[dr][[:alpha:]]$`)
+// ataRE matches an ATA path component, capturing the ATA print ID.
+var ataRE = regexp.MustCompile(`^ata([[:digit:]]+)$`)
 
-var ataDevRE = regexp.MustCompile(`^dev[[:digit:]]+\.([[:digit:]]+)\.?([[:digit:]]*)`)
-
-func handleATADevicePathNode(builder devicePathBuilder) error {
-	if builder.numRemaining() < 6 {
-		return errors.New("invalid path: not enough components")
-	}
-
-	m := ataRE.FindStringSubmatch(builder.next(6))
-	if len(m) == 0 {
-		return fmt.Errorf("invalid path components: %s", builder.next(6))
-	}
-
-	// Obtain the ATA port number local to this ATA controller.
-	portBytes, err := ioutil.ReadFile(filepath.Join(builder.absPath(builder.next(1)), "ata_port", builder.next(1), "port_no"))
-	if err != nil {
-		return xerrors.Errorf("cannot obtain port ID: %w", err)
-	}
-
-	lun, err := strconv.ParseUint(m[4], 10, 16)
-	if err != nil {
-		return xerrors.Errorf("invalid LUN: %w", err)
-	}
-
-	var node efi.DevicePathNode
-
-	switch builder.interfaceType() {
-	case interfaceTypeIDE:
-		controller, err := strconv.Atoi(strings.TrimSpace(string(portBytes)))
-		if err != nil {
-			return xerrors.Errorf("invalid controller: %w", err)
-		}
-		// PATA has a maximum of 2 ports.
-		if controller < 1 || controller > 2 {
-			return fmt.Errorf("invalid controller: %d", controller)
-		}
-		// The channel is always 0 for PATA devices (no port multiplier).
-		if m[2] != "0" {
-			return errors.New("invalid channel")
-		}
-		// The target corresponds to the PATA device.
-		drive, err := strconv.ParseUint(m[3], 10, 1)
-		if err != nil {
-			return xerrors.Errorf("invalid drive: %w", err)
-		}
-
-		node = &efi.ATAPIDevicePathNode{
-			Controller: efi.ATAPIControllerRole(controller - 1),
-			Drive:      efi.ATAPIDriveRole(drive),
-			LUN:        uint16(lun)}
-	case interfaceTypeSATA:
-		port, err := strconv.ParseUint(strings.TrimSpace(string(portBytes)), 10, 16)
-		if err != nil {
-			return xerrors.Errorf("invalid port ID: %w", err)
-		}
-
-		printId, _ := strconv.Atoi(m[1])
-
-		// The channel indicates the port multiplier port number for SATA devices.
-		pmp, err := strconv.ParseUint(m[2], 10, 15)
-		if err != nil {
-			return xerrors.Errorf("invalid PMP: %w", err)
-		}
-		// SATA ports only have a single device, so the target should be zero.
-		if m[3] != "0" {
-			return errors.New("invalid target")
-		}
-
-		// We need to determine if the device is connected via a port
-		// multiplier because we have to set the PMP address to 0xffff
-		// if it isn't. Unfortunately, it is zero indexed so checking
-		// that it is zero isn't sufficient.
-		//
-		// The kernel will expose a single host link%d device if there
-		// is no port multiplier, or one of more PMP link%d.%d devices
-		// if there is a port multiplier attached (see
-		// drivers/ata/libata-pmp.c:sata_pmp_init_links and
-		// drivers/ata/libata-transport.c:ata_tlink_add).
-		_, err = os.Stat(filepath.Join(builder.next(1), fmt.Sprintf("link%d.%d", printId, pmp)))
-		switch {
-		case os.IsNotExist(err):
-			// No port multiplier is connected.
-			pmp = 0xffff
-		case err != nil:
-			return err
-		default:
-			// A port multiplier is connected.
-		}
-
-		node = &efi.SATADevicePathNode{
-			// The kernel provides a one-indexed number and the firmware is zero-indexed.
-			HBAPortNumber:            uint16(port) - 1,
-			PortMultiplierPortNumber: uint16(pmp),
-			LUN:                      uint16(lun)}
-	default:
-		return errors.New("invalid interface type")
-	}
-
-	builder.advance(6)
-	builder.append(node)
-	return nil
+type ataParams struct {
+	printId uint32
+	port    uint32
+	*scsiParams
 }
 
-func init() {
-	registerDevicePathNodeHandler("ata", handleATADevicePathNode, 0, interfaceTypeIDE, interfaceTypeSATA)
+func handleATAPath(path string) (*ataParams, error) {
+	components := strings.Split(path, string(os.PathSeparator))
+	if len(components) < 6 {
+		return nil, errors.New("invalid path: insufficient components")
+	}
+
+	ata := components[len(components)-6]
+	m := ataRE.FindStringSubmatch(ata)
+	if len(m) == 0 {
+		return nil, fmt.Errorf("invalid path component: %s", ata)
+	}
+
+	scsiParams, err := handleSCSIPath(path)
+	if err != nil {
+		return nil, err
+	}
+
+	printId, err := strconv.ParseUint(m[1], 10, 32)
+	if err != nil {
+		return nil, xerrors.Errorf("invalid print ID: %w", err)
+	}
+
+	// Obtain the ATA port number local to this ATA controller. The kernel
+	// creates one ata%d device per port (see drivers/ata/libata-core.c:ata_host_register).
+	portBytes, err := ioutil.ReadFile(filepath.Join(path, "../../../../..", "ata_port", ata, "port_no"))
+	if err != nil {
+		return nil, xerrors.Errorf("cannot obtain port ID: %w", err)
+	}
+	port, err := strconv.ParseUint(strings.TrimSpace(string(portBytes)), 10, 16)
+	if err != nil {
+		return nil, xerrors.Errorf("invalid port ID: %w", err)
+	}
+
+	return &ataParams{
+		printId:    uint32(printId),
+		port:       uint32(port),
+		scsiParams: scsiParams}, nil
 }
--- linux/dp_scsi.go
+++ linux/dp_scsi.go
@@ -7,44 +7,83 @@ package linux
 import (
 	"errors"
 	"fmt"
+	"math"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/canonical/go-efilib"
 
 	"golang.org/x/xerrors"
 )
 
-var scsiRE = regexp.MustCompile(`^host[[:digit:]]+\/target[[:digit:]]+\:[[:digit:]]+\:[[:digit:]]+\/[[:digit:]]+\:([[:digit:]]+)\:([[:digit:]])+\:([[:digit:]])+\/block\/s[dr][[:alpha:]]$`)
+// scsiRE matches a SCSI path, capturing the channel, target and LUN.
+var scsiRE = regexp.MustCompile(`^host[[:digit:]]+\/target[[:digit:]]+\:[[:digit:]]+\:[[:digit:]]+\/[[:digit:]]+\:([[:digit:]]+)\:([[:digit:]]+)\:([[:digit:]]+)\/block\/s[dr][[:alpha:]]$`)
+
+type scsiParams struct {
+	channel uint32
+	target  uint32
+	lun     uint64
+}
+
+func handleSCSIPath(path string) (*scsiParams, error) {
+	components := strings.Split(path, string(os.PathSeparator))
+	if len(components) < 5 {
+		return nil, errors.New("invalid path: insufficient components")
+	}
+
+	path = filepath.Join(components[len(components)-5:]...)
+	m := scsiRE.FindStringSubmatch(path)
+	if len(m) == 0 {
+		return nil, fmt.Errorf("invalid path components: %s", path)
+	}
+
+	channel, err := strconv.ParseUint(m[1], 10, 32)
+	if err != nil {
+		return nil, xerrors.Errorf("invalid channel: %w", err)
+	}
+	target, err := strconv.ParseUint(m[2], 10, 32)
+	if err != nil {
+		return nil, xerrors.Errorf("invalid target: %w", err)
+	}
+	lun, err := strconv.ParseUint(m[3], 10, 64)
+	if err != nil {
+		return nil, xerrors.Errorf("invalid lun: %w", err)
+	}
+
+	return &scsiParams{
+		channel: uint32(channel),
+		target:  uint32(target),
+		lun:     lun}, nil
+}
 
 func handleSCSIDevicePathNode(builder devicePathBuilder) error {
 	if builder.numRemaining() < 5 {
-		return errors.New("invalid path: not enough components")
+		return errors.New("invalid path: insufficient components")
 	}
 
-	m := scsiRE.FindStringSubmatch(builder.next(5))
-	if len(m) == 0 {
-		return fmt.Errorf("invalid path components: %s", builder.next(5))
+	params, err := handleSCSIPath(builder.absPath(builder.next(5)))
+	if err != nil {
+		return err
 	}
 
 	builder.advance(5)
 
-	if m[1] != "0" {
+	if params.channel != 0 {
 		return errors.New("invalid channel")
 	}
-
-	target, err := strconv.ParseUint(m[2], 10, 16)
-	if err != nil {
-		return xerrors.Errorf("invalid target: %w", err)
+	if params.target > math.MaxUint16 {
+		return errors.New("invalid target")
 	}
-	lun, err := strconv.ParseUint(m[3], 10, 16)
-	if err != nil {
-		return xerrors.Errorf("invalid LUN: %w", err)
+	if params.lun > math.MaxUint16 {
+		return errors.New("invalid LUN")
 	}
 
 	builder.append(&efi.SCSIDevicePathNode{
-		PUN: uint16(target),
-		LUN: uint16(lun)})
+		PUN: uint16(params.target),
+		LUN: uint16(params.lun)})
 	return nil
 }
 
